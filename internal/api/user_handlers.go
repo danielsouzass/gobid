f9ceafd0ca699493a6d45944ee7b581a262db117
Daniel Souza <danielsouza51764@gmail.com.br>
2025-11-13 10:41:34 -0300

    Estrutura da API e Rotas com BindRoutes

---
+++ internal/api/user_handlers.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (api *API) handleSignupUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
+
+func (api *API) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
+
+func (api *API) handleLogoutUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
